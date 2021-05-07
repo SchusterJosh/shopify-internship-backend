4fbd6b63e3e0a5ec87ab634ee2e6ac8c4e1c0809
Josh <joshuaschusterjs@gmail.com>
2021-05-07 13:04:51 -0400

    Creating new package.json file for heroku deployment.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 // Define routes
-app.use('/api/search', require('./routes/api'));
+app.use("/api/search", require("./routes/api"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
